4e7709539d7e92a7e8bce3f2a65c1a387478718e
jakkrich1234 <thesthxngcakrkrich@gmail.com>
2024-12-23 14:52:21 +0700

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,24 @@
+x =1000
+
+A = 150
+B = 105
+C = 120
+D = 6.50
+E = 150
+F = 20
+G = 10
+H = 18
+I = 10.25
+J = 120.25
+K = 21.50
+L = 29.50
+M = 15.75
+N = 19
+O = 11.50
+P = 15.15
+Q = 10
+
+price = (A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q )
+
+print(f'torn{x - price:.2f} Bath')
+print(f'Price is {price} Bath.')
